36381d03e45ed2aeab1468115b32002969ef1d2e
xookerchang <e84991211@gmail.com>
2020-11-11 18:44:52 +0800

    adding reading funciton

--- product.py
+++ product.py
@@ -1,4 +1,16 @@
-products =[]
+
+
+products = []
+with open('products.csv', 'r', encoding = 'utf-8'):
+	for line in f:
+		if '商品,價格' in line:
+			continue # 繼續
+		name, price = line.strip().split(',')  # strip delete /n ; split切割完後結果是清單 restore name, price
+		products.append([name , price])
+print(products)
+
+# 讓使用者輸入
+
 while True:
 	name = input('商品名稱：')
 	if name == 'q':
@@ -8,8 +20,15 @@ while True:
 	products.append([name, price])
 print(products)
 
+# 印出所有購買紀錄
 for i in products:
 	print(i[0], '的商品價格是:', i[1])
+
+# 寫入coding 
+with open('products.txt', 'w', encoding ='utf-8') as f:
+	f.write('商品,價格\n') # \n下一行
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')  # ','可以做為區隔
 
 
 
